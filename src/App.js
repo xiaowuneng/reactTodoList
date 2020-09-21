50003cb47c56b6c9132c28bcd299aeeeaf9747ea
benny <benny@192.168.0.102>
2020-09-21 21:52:45 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Input,Button,List } from 'antd';
+import {getChangeInputValueAction,getAddTodoItemAction,getDeleteTodoItemAction} from './store/actionCreators'
+import store from './store'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super()
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
+    this.state = store.getState()
+    store.subscribe(this.handleStoreChange)
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Input
+          placeholder={'todo'}
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+          style={{width: "600px",marginRight:"10px"}} />
+        <Button type="primary" onClick={this.handleBtnClick}>Primary</Button>
+        <List
+        style={{width: "600px",marginTop:"10px"}}
+        bordered
+        dataSource={this.state.list}
+        renderItem={(item, index) => ( <List.Item onClick={this.handleItemDelete.bind(this,index)}>{item}</List.Item>)}
+      />
+      </div>
+    );
+  }
+
+  handleInputChange (e) {
+    const action = getChangeInputValueAction(e.target.value)
+    store.dispatch(action)
+  }
+
+  handleBtnClick () {
+    if(!this.state.inputValue) {
+      alert('add todo item')
+      return
+    }
+    const action = getAddTodoItemAction()
+    store.dispatch(action)
+  }
+
+  handleItemDelete (index) {
+    const action = getDeleteTodoItemAction(index)
+    store.dispatch(action)
+  }
+
+  handleStoreChange () {
+    this.setState(store.getState())
+  }
 }
 
 export default App;
---
+++ src/store/actionCreators.js
@@ -0,0 +1,12 @@
+import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './actiontypes'
+export const getChangeInputValueAction = (value) => ({
+  type: CHANGE_INPUT_VALUE,
+  value
+})
+export const getAddTodoItemAction = () => ({
+  type: ADD_TODO_ITEM
+})
+export const getDeleteTodoItemAction = (index) => ({
+  type: DELETE_TODO_ITEM,
+  index
+})
